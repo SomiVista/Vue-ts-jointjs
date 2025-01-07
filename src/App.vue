7b0a2a4b284c5d38c62e68d6a10ec42af5d1c713
SomiVista <somayerohani06@gmail.com>
2025-01-07 15:45:53 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { dia, shapes } from "@joint/core";
+import { onMounted } from "vue";
+
+function setupJointJs() {
+  const namespace = shapes;
+
+  const graph = new dia.Graph({}, { cellNamespace: namespace });
+
+  const paper = new dia.Paper({
+    el: document.getElementById("paper"),
+    model: graph,
+    width: document.body.clientWidth,
+    height: document.body.clientHeight,
+    background: { color: "red" },
+    cellViewNamespace: namespace,
+  });
+}
+
+onMounted(() => {
+  setupJointJs();
+});
+</script>
+
+<template>
+  <div id="paper"></div>
+</template>
+
+<style scoped></style>
