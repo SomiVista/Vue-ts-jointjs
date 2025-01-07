36c63a0e41b08133503cb08b786707f10f963b50
SomiVista <somayerohani06@gmail.com>
2025-01-07 16:01:14 +0300

    initial a Rectangle in central of window

--- src/App.vue
+++ src/App.vue
@@ -6,15 +6,27 @@ function setupJointJs() {
   const namespace = shapes;
 
   const graph = new dia.Graph({}, { cellNamespace: namespace });
+  const paperWidth = document.body.clientWidth;
+  const paperHeight = document.body.clientHeight;
 
   const paper = new dia.Paper({
     el: document.getElementById("paper"),
     model: graph,
-    width: document.body.clientWidth,
-    height: document.body.clientHeight,
-    background: { color: "red" },
+    width: paperWidth,
+    height: paperHeight,
+    background: { color: "#f5f5f5" },
     cellViewNamespace: namespace,
   });
+
+  const rect1 = new shapes.standard.Rectangle();
+  const rect1Width = 180;
+  const rect1Height = 50;
+  rect1.position(
+    (paperWidth - rect1Width) / 2,
+    (paperHeight - rect1Height) / 2
+  );
+  rect1.resize(rect1Width, rect1Height);
+  rect1.addTo(graph);
 }
 
 onMounted(() => {
